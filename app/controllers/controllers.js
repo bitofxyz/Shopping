1e53d229d20b23bddd9d926ba49c638bd823be23
Yinfeng Guo <guoyinfeng@live.com>
2014-04-26 12:12:29 +0800

    calculate total

--- app/controllers/controllers.js
+++ app/controllers/controllers.js
@@ -3,12 +3,24 @@
  */
 function CartController($scope){
     $scope.products = [
-        {title: 'Paint pots', quantity: 8, price: 3.95},
-        {title: 'Polka dots', quantity: 17, price: 12.95},
-        {title: 'Pebbles', quantity: 5, price: 6.95}
+        {title: 'Paint pots', quantity: 8, price: 1},
+        {title: 'Polka dots', quantity: 17, price: 2},
+        {title: 'Pebbles', quantity: 5, price: 3}
     ];
+    $scope.totalMoney = 0;
 
     $scope.remove = function(index) {
         $scope.products.splice(index, 1);
     };
+
+    $scope.computeTotalMoney = function(){
+        console.log('products::'+$scope.products);
+        $scope.totalMoney = 0;
+        for(var i=0; i<$scope.products.length; i++){
+            $scope.totalMoney += $scope.products[i].price * $scope.products[i].quantity;
+        }
+        console.log(i);
+    }
+
+    $scope.$watch('products', $scope.computeTotalMoney, true);
 }
